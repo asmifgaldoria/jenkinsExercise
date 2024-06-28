00f978135e94179a888df56b59aa5bbdb5db0b98
Luigi Stc <l.parzybut@gmail.com>
2024-06-28 12:41:20 +0200

    jenkinsfile added

--- main/fun_to_test.py
+++ main/fun_to_test.py
@@ -1,10 +1,27 @@
+"""This is module"""
+
 def simple_return(text: str):
+    """
+    This is simple return
+    :param text:
+    :return:
+    """
     return text
 
 
 def simple_upper(text: str):
+    """
+    This is simple_upper
+    :param text:
+    :return:
+    """
     return text.upper()
 
 
 def simple_power(num: int):
+    """
+    This is simple_power
+    :param num:
+    :return:
+    """
     return pow(num, 2)
